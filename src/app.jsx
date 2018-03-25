4edeaa29dd4a73d096b1ee66b8e114dd078bfc0c
nyan <sibaoemail@126.com>
2018-03-25 13:29:25 +0800

    ant table

--- src/app.jsx
+++ src/app.jsx
@@ -8,6 +8,7 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import AppLayout from "page/layout/AppLayout.jsx";
 import Home from "page/home/Home.jsx";
 import Login from "page/login/Login.jsx";
+import AntTable from "page/ant-table/AntTable.jsx";
 import ErrorPage from "page/error-page/ErrorPage.jsx";
 
 import 'antd/dist/antd.less';
@@ -26,6 +27,7 @@ class App extends React.Component {
                         <AppLayout>
                             <Switch>
                                 <Route path="/" exact component={Home}/>
+                                <Route path="/antTable" exact component={AntTable}/>
                                 <Route component={ErrorPage}/>
                             </Switch>
                         </AppLayout>
--- src/component/sidebar/SideBar.js
+++ src/component/sidebar/SideBar.js
@@ -36,14 +36,22 @@ class SideBar extends React.Component {
                         </Menu.Item>
                     </SubMenu>
                     <SubMenu key="sub2" title={<span><Icon type="save"/>商品</span>}>
-                        <Menu.Item key="6">商品管理</Menu.Item>
-                        <Menu.Item key="7">品类管理</Menu.Item>
+                        <Menu.Item key="6">
+                            <Link to="/product">商品管理</Link>
+                        </Menu.Item>
+                        <Menu.Item key="7">
+                            <Link to="/category">品类管理</Link>
+                        </Menu.Item>
                     </SubMenu>
                     <SubMenu key="sub3" title={<span><Icon type="shopping-cart"/>订单</span>}>
-                        <Menu.Item key="8">订单管理</Menu.Item>
+                        <Menu.Item key="8">
+                            <Link to="/order">订单管理</Link>
+                        </Menu.Item>
                     </SubMenu>
                     <SubMenu key="sub4" title={<span><Icon type="user"/>用户</span>}>
-                        <Menu.Item key="9">用户管理</Menu.Item>
+                        <Menu.Item key="9">
+                            <Link to="/user">用户管理</Link>
+                        </Menu.Item>
                     </SubMenu>
 
                 </Menu>
